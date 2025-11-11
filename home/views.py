0ab08bef972113c34e2cd2690cc18bc751c730e5
Sara Maria Alves de Lima <saramalima82@gmail.com>
2025-11-11 00:05:37 -0300

    Configurações iniciais do ambiente para iniciar a página

--- home/views.py
+++ home/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
     return render(request, 'home/pages/home.html')
+def cadastro(request):
+    return render(request, 'home/pages/cadastro.html')
