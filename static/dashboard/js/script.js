bb76284c7b839e44a78510f6249773fb0ca5779d
Sara Maria Alves de Lima <saramalima82@gmail.com>
2025-11-17 09:47:48 -0300

    Resolvido o problema da navegação entre página

--- static/dashboard/js/script.js
+++ static/dashboard/js/script.js
@@ -1,13 +1,13 @@
 document.addEventListener('DOMContentLoaded', function(){
     // Deixa a página atual como "active" se baseando no nome do arquivo
-    const path = location.pathname.split('/').pop(); // Procura o caminho do arquivo e pega ele
-    const page = path === '' ? 'dashboard-alunos' : path;
-    const tabs = document.querySelectorAll('.tab');
-    tabs.forEach(t => {
-        const href = t.getAttribute('href');
-        if(!href) return;
-        if(href.includes(page)) t.classList.add('active');
-    });
+    // const path = location.pathname.split('/').pop().toLowerCase(); // Procura o caminho do arquivo e pega ele
+    // const page = path === '' ? 'dashboard-alunos' : path;
+    // const tabs = document.querySelectorAll('.tab');
+    // tabs.forEach(t => {
+    //     const href = t.getAttribute('href');
+    //     if(!href) return;
+    //     if(href.includes(page)) t.classList.add('active');
+    // });
     
     // Barrinha de progresso
     document.querySelectorAll('.progress-fill').forEach(el => {
