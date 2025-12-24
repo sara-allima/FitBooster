c1bb05e2e6d906c860d888d7deac5dd846fa1904
Sara Maria Alves de Lima <saramalima82@gmail.com>
2025-12-24 10:51:30 -0300

    Model do Treinador

--- home/models.py
+++ home/models.py
@@ -1,45 +1,44 @@
-from django.contrib.auth.models import User
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
-class Treino(models.Model):
-    treino_id = models.IntegerField(primary_key=True)
-    #aluno_id = models.ForeignKey(
-        #Tem que ver como pega isso do User que vai ser o aluno   
-    #)
-    #treinador_id = models.ForeignKey(
-        #Mesma coisa que o aluno_id   
-    #)
-    nome_treino = models.CharField(max_length=100)
-    descricao = models.TextField()
-    data_criacao = models.DateField(auto_now_add=True)
-    tipo_treino = models.CharField(max_length=30)
-    exercicios = models.ManyToManyField(
-        Exercicio,
-        through='TreinoExercicio'
+class Treinador(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    cref = models.CharField(primary_key=True, max_length=20)
+    nome = models.CharField(max_length=100)
+    genero = models.CharField(max_length=20, choices=[
+        ('Masculino'),
+        ('Feminino'),
+        ('Outro')
+    ])
+    email = models.EmailField(unique=True)
+    cpf = models.CharField(max_length=11, unique=True)
+    formacao = models.TextField()
+    idade = models.IntegerField()
+
+    def __str__(self):
+        return self.nome or self.user.username
+
+class Aluno(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    nome = models.CharField(max_length=100)
+    genero = models.CharField(max_length=20, choices=[
+        ('Masculino'),
+        ('Feminino'),
+        ('Outro')
+    ])
+    email = models.EmailField(unique=True)
+    objetivo = models.CharField(max_length=100)
+    peso = models.DecimalField(max_digits=5, decimal_places=2)
+    altura = models.DecimalField(max_digits=4, decimal_places=2)
+    idade = models.IntegerField()
+    treinador = models.ForeignKey(
+        Treinador,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='alunos'
     )
 
     def __str__(self):
-        return self.nome_treino
-
-class Exercicio(models.Model):
-    exercicio_id = models.IntegerField(primary_key=True)
-    nome_exercicio = models.CharField(max_length=25)
-    grupo_muscular = models.CharField(max_length=50)
-    descricao = models.TextField()
-    series = models.IntegerField(default=3)
-    repeticoes = models.IntegerField(default=12)
-    carga = models.IntegerField(default=1)
-
-    def __str__(self):
-        return self.nome_exercicio
-
-class TreinoExercicio(models.Model):
-    treino_id = models.ForeignKey(
-        Treino,
-        on_delete=models.CASCADE
-    )
-    exercicio_id = models.ForeignKey(
-        Exercicio,
-        on_delete=models.CASCADE
-    )
+        return self.nome or self.user.username
