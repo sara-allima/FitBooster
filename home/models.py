a98e8597d5a55d33e752ba4c3797537ad7893b06
Sara Maria Alves de Lima <saramalima82@gmail.com>
2025-12-24 11:25:14 -0300

    Models de Treino e Exercício

--- home/models.py
+++ home/models.py
@@ -42,3 +42,40 @@ class Aluno(models.Model):
 
     def __str__(self):
         return self.nome or self.user.username
+
+class Exercicio(models.Model):
+    nome = models.CharField(max_length=100)
+    grupo_muscular = models.CharField(max_length=50)
+    descricao = models.TextField()
+    series = models.IntegerField(default=3)
+    repeticoes = models.IntegerField(default=12)
+    carga = models.IntegerField(default=0, help_text='Carga em Kg')
+    
+    def __str__(self):
+        return f'{self.nome} - {self.grupo_muscular}'
+    
+class Treino(models.Model):
+    nome = models.CharField(max_length=100)
+    descricao = models.TextField()
+    tipo = models.CharField(max_length=50)
+    data_criacao = models.DateField(auto_now_add=True)
+    treinador = models.ForeignKey(
+        Treinador,
+        on_delete=models.CASCADE,
+        related_name='treinos_criados'
+    )
+    alunos = models.ManyToManyField(
+        Aluno,
+        through='AlunoTreino',
+        through_fields=('treino', 'aluno'),
+        related_name='treinos'
+    )
+    exercicios = models.ManyToManyField(
+        Exercicio,
+        through='TreinoExercicio',
+        through_fields=('treino', 'exercicio'),
+        related_name='treinos'
+    )
+
+    def __str__(self):
+        return f'{self.nome} - {self.tipo}'
