1df1c70bad8d13f3a3f635dd5d8afe5124ae2953
Sara Maria Alves de Lima <saramalima82@gmail.com>
2026-01-02 20:07:23 -0300

    Ajustando formulário do TREINADOR para mandar dados para banco de dados

--- home/views.py
+++ home/views.py
@@ -1,13 +1,59 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.db import IntegrityError
+from django.contrib.auth.models import User
+from .models import Treinador
 
 # Create your views here.
 def home(request):
     return render(request, 'home/pages/home.html')
+
 def cadastro(request):
     return render(request, 'home/pages/cadastro.html')
+
 def formulario_treinador(request):
+    if request.method == 'POST':
+        cref = request.POST.get('cref')
+        nome = request.POST.get('nome')
+        genero = request.POST.get('genero')
+        email = request.POST.get('email')
+        cpf = request.POST.get('cpf')
+        formacao = request.POST.get('formacao')
+        idade = request.POST.get('idade')
+        senha = request.POST.get('senha')
+        confirmar_senha = request.POST.get('confirmar_senha')
+
+        if senha != confirmar_senha:
+            return render(request, 'home/pages/formularioTreinador.html', {
+                'erro': 'As senhas não coincidem.'
+            })
+
+        try:
+            user = User.objects.create_user(
+                username=nome,
+                email=email,
+                password=senha
+            )
+            treinador = Treinador.objects.create(
+                user=user,
+                nome=nome,
+                email=email,
+                cpf=cpf,
+                idade=idade,
+                genero=genero,
+                cref=cref,
+                formacao=formacao
+            )
+
+            return redirect('home/pages/dashboardAlunos.html')
+        
+        except IntegrityError:
+            return render(request, 'home/pages/formularioTreinador.html', {
+                'erro': 'Erro ao cadastrar treinador. Email ou CPF já cadastrados.'
+            })
+        
     return render(request, 'home/pages/formularioTreinador.html')
+
 def dashboard_alunos(request):
     return render(request, 'home/pages/dashboardAlunos.html')
 def dashboard_planos_treino(request):
