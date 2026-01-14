11faadf7ded682c219eef7b1a6d16e080144dc95
Everton gean <henriqueuna2004@gmail.com>
2026-01-14 18:57:01 -0300

    conectar-treinador-front

--- core/views.py
+++ core/views.py
@@ -48,3 +48,6 @@ def treinoa(request):
 
 def calendario(request):
     return render(request, 'core/calendario.html')
+
+def treinadores(request):
+    return render(request, 'core/treinadores.html')
